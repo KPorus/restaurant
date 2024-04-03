d9112126ad26b94446a3f94e0c3ff43462c6cf9f
KPorus <designerfardin2021@gmail.com>
2024-04-03 12:18:52 +0600

    ingredient update

--- src/components/Home/HomeIngredient.tsx
+++ src/components/Home/HomeIngredient.tsx
@@ -3,7 +3,7 @@ import style from "@/styles/HomeIngredient/homeIngredient.module.scss"
 const HomeIngredient = () => {
     return (
         <div className={`${style.ingredient}`}>
-            <div className='row'>
+            <div className='row' style={{display:"flex"}}>
                 <div className='col-1-of-2'></div>
                 <div className={`col-1-of-2 ${style.ingredientBody}`}>
                     <h1 className={`${style.ingredientTitle}`}>Fine ingredients</h1>
